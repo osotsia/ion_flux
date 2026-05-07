f9b06ca6fc44db5407773bf263db3ca206ff4633
osotsia <osotsi.a@gmail.com>
2026-05-07 12:36:10 -0400

    CI/CD

--- models/ORegan2022_ThermalDFN.py
+++ models/ORegan2022_ThermalDFN.py
@@ -11,7 +11,7 @@ It leverages Async Task Parallelism to execute multiple state-machine protocols
 concurrently, and extracts internal spatial fields to diagnose rate-limiting transport phenomena.
 
 TODO: change the current uniform mesh to exponential
-TODO: this model doesn't run yet, still in progress
+TODO: this model doesn't run yet, still under construction
 """
 
 import math
